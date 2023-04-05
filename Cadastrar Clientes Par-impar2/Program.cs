67c5bc4c965bfbfa14bb006d085e2f31d3b30041
washington1313 <123412352+washington1313@users.noreply.github.com>
2023-04-04 23:43:25 -0300

    Add files via upload

---
+++ Cadastrar Clientes Par-impar2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cadastrar_Clientes_Par_impar2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nomesClientes = new string[6]; 
+            {
+                for (int i = 0; i < nomesClientes.Length; i++) 
+                {
+                    Console.WriteLine("Digite o Nome do Cliente;");
+                    nomesClientes[i]= Console.ReadLine();
+                    Console.Clear();
+                }
+
+                for (int i = 0;i < nomesClientes.Length;i++)
+                {
+                    if (i % 2 == 0) 
+                    {                        
+                        Console.WriteLine(nomesClientes[i]);                        
+                    }
+                }
+
+                for (int i = 0; i < nomesClientes.Length; i++)
+                {
+                    if (i % 2 == 1)
+                    {                       
+                        Console.WriteLine(nomesClientes[i]);
+                    }
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+}
